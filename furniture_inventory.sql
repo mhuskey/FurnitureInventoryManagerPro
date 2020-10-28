8bec2eee3c2f163b7629be0613ad39fdbb665255
Matthew Huskey <30731520+mhuskey@users.noreply.github.com>
2020-10-28 16:19:41 -0400

    furniture_inventory.sql
    • Added the `furniture_inventory.sql` file to show the structure of the database and for sample data to import

---
+++ furniture_inventory.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: furniture_inventory
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `furniture`
+--
+
+DROP TABLE IF EXISTS `furniture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `furniture` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `stock` int NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `weight_lbs` decimal(7,2) NOT NULL,
+  `cubes` decimal(7,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `furniture`
+--
+
+LOCK TABLES `furniture` WRITE;
+/*!40000 ALTER TABLE `furniture` DISABLE KEYS */;
+INSERT INTO `furniture` VALUES (1,'Ashley Furniture','B553BR King Bedroom',7,'Bedroom',3798.00,1068.35,175.89),(2,'Ashley Furniture','B719BR Queen Bedroom',1,'Bedroom',2456.00,796.60,111.86),(3,'Ashley Furniture','D553DR Dining Room',0,'Dining Room',3670.00,1140.80,139.73);
+/*!40000 ALTER TABLE `furniture` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-28 16:17:58
