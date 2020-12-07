db6c55f52b9a9980e0474ef50e6911a8c4d3ff44
Matthew Huskey <matthewhuskey@me.com>
2020-12-07 00:30:49 -0500

    Back Links for Admin CRUD Pages
    • Added links back to `public/staff/admins/index.php` and basic structure for the Delete, Edit, and New Admin pages

--- public/staff/admins/delete.php
+++ public/staff/admins/delete.php
@@ -5,4 +5,23 @@
 <?php $page_title = 'Delete Admin'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
+    <!-- Main Content -->
+    <main role="main">
+      <section>
+        <div class="main-content">
+          <div class="container min-vh-100">
+            <div class="row">
+              <div class="col-sm-10 offset-sm-1 text-center">
+                <h1>Delete Admin</h1>
+                
+                <h5><a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>"> <i class="fas fa-chevron-circle-left"></i> Back to Admins</a></h5><br />
+                
+                <p>Are you sure you want to delete this admin?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
--- public/staff/admins/edit.php
+++ public/staff/admins/edit.php
@@ -5,4 +5,22 @@
 <?php $page_title = 'Edit Admin'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
+    <!-- Main Content -->
+    <main role="main">
+      <section>
+        <div class="main-content">
+          <div class="container min-vh-100">
+            <div class="row">
+              <div class="col-sm-10 offset-sm-1 text-center">
+                <h1>Edit Admin</h1>
+                
+                <h5><a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>"> <i class="fas fa-chevron-circle-left"></i> Back to Admins</a></h5><br />
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
